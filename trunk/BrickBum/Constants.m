6e8a2244a5df9b53082d157c5ecc9c10f431941d
OGU <OGU@b4c86669-cdde-b240-8665-79a095c818af>
2016-02-17 22:40:42 +0000

    High Score UTF8 Character sending problem solved Touch sensitivity of brick couples are improved

--- trunk/BrickBum/Constants.m
+++ trunk/BrickBum/Constants.m
@@ -12,5 +12,5 @@
 NSString *const WebSite                  = @"http://brickbomb.ozoggames.com/";
 NSString *const GameName                 = @"BrickBomb";
 NSString *const AppId                    = @"1082150144";
-NSString *const ApiAddress               = @"http://brickbomb.ozoggames.com/api/";
+NSString *const ApiAddress               = @"http://brickbomb.ozoggames.com/api";
 @end
